42dc15e2ef8915c424319f6ba57d4a6598b75e8c
switch620 <switch620@gmail.com>
2017-11-29 19:53:50 -0800

    Improved readability and got rid of unnecessary things.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/lib/python3
 import sklearn
 import xgboost
 from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
@@ -11,8 +12,9 @@ import matplotlib
 from datetime import datetime as dt
 
 
-FILE = 'C:\git\AI_regression_assignment\Dataset.csv'
-TEST_SIZE = .05
+# FILE = 'C:\git\AI_regression_assignment\Dataset.csv'
+FILE = 'Dataset.csv'
+TEST_SIZE = .25
 
 
 def main():
@@ -25,17 +27,19 @@ def main():
     dataframe = None
     with open(FILE) as file:
         csv_data = csv.reader(file)
-        ####    Note: Not using 'Pressure'
-        # dataframe = pd.read_csv(file, usecols=['Temperature', 'ThermalConductivity', 'SoundVelocity', 'O1', 'O2'])
         dataframe = pd.read_csv(file, usecols=['Temperature', 'ThermalConductivity', 'O1', 'O2'])
+        ####    Note: Not using 'Pressure' or 'SoundVelocity' -- This is because they are creating noise.
+	####		I found this out through use of RFECV, a tool to aid in Feature Selection
 
-
+    ####    'num' used to calculate which columns to use from dataframe
+    ####    (we are subtracting 2 because we are predicting two things, O1 and O2)
     num = len(dataframe.columns) - 2
     X = dataframe.as_matrix(columns=dataframe.columns[0:num])
-    # y1 = dataframe.O1
     y1 = numpy.asarray(dataframe.O1)
-    # y2 = dataframe.O2
     y2 = numpy.asarray(dataframe.O2)
+    ####    Old way I was grabbing data, not as nice to use when displaying output-- Use numpy variation abouve
+    # y1 = dataframe.O1
+    # y2 = dataframe.O2
 
 
     ####    Was testing out scaling and/or normalizing data--   Did not improve accuracy
@@ -43,48 +47,47 @@ def main():
     # y1 = sklearn.preprocessing.scale(y1)
     # y2 = sklearn.preprocessing.scale(y2)
 
+
     ####    These aren't as accurate
     # clf = LinearRegression()
     # clf = Lasso()
-
-    ####    More accurate
+    ####    More accurate algorithms
+    # clf = AdaBoostRegressor(base_estimator=CLF, n_estimators=100, learning_rate=1)
     # clf = RandomForestRegressor(n_estimators=17, criterion="mse", max_features=None)
-    # clf = xgboost.XGBRegressor()
-    clf = xgboost.XGBRegressor(n_estimators=800, learning_rate=0.02, max_depth=20)
-    print(clf)
+    clf = xgboost.XGBRegressor(n_estimators=800, learning_rate=0.1, max_depth=30)
+
 
     ####    This helps for feature selection
     # clf = RFECV(estimator=clf, step=1)
 
+
+    print("Classifier being used: {}".format(clf))
+    ####    For scoring and timing
     score, iterations = 0, 0
     t0 = dt.today().now()
 
     for i in range(40):
-        print('\nTest {}'.format(i+1))
-        X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size=TEST_SIZE)
 
-        clf.fit(X_train,y_train)
-
+	####	Following 3 lines are used for Feature Selection process only
         # clf.fit_transform(X_train, y_train)
         # print('Supporting features: {}'.format(clf.support_))
         # print('Feature rankings: {}'.format(clf.ranking_))
 
-        # clf = AdaBoostRegressor(base_estimator=CLF, n_estimators=100, learning_rate=1)
-        # clf.fit(X_train, y_train)
-
+        print('\nTest {}'.format(i+1))
+        X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size=TEST_SIZE)
+        clf.fit(X_train,y_train)
 
         score += clf.score(X_test, y_test)
+        iterations += 1
+        t1 = dt.today().now()
+
+
         # print("Input: {}".format(X_test))
         # print("Predictions: {}".format(clf.predict(X_test)))
         # print("Actual: {}".format(y_test))
-        iterations += 1
-        print('Average score: ', score/iterations)
-
-        t1 = dt.today().now()
-        print('Time:\t{} seconds'.format(t1-t0))
-
+        print('Average Score: ', score/iterations)
+        print('Total Time:\t{} seconds'.format(t1-t0))
         # print('Feature importance: {}'.format(clf.feature_importances_))
-
 
 
 if __name__ == '__main__':
